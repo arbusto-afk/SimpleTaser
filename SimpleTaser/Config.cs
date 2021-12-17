64d3ffde6a6404717716a9ab4458a30b74a0596f
Arbusto <88610004+arbusto-afk@users.noreply.github.com>
2021-12-17 13:32:15 -0300

    Add files via upload

---
+++ SimpleTaser/Config.cs
@@ -0,0 +1,16 @@
+﻿using Rocket.API;
+
+namespace SimpleTaser
+{
+    public class Config : IRocketPluginConfiguration
+    {
+        public int TaserId;
+        public int TaserTime;
+
+        public void LoadDefaults()
+        {
+            TaserId = 7471;
+            TaserTime = 6;
+        }
+    }
+}
